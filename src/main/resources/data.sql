3bf7911f29a5e019ad020a72371600d33aca96a9
Sudip-byte <sudiprana9729@gmail.com>
2020-06-05 23:45:08 +0530

    h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO OKAYCOMPUTING (name, city, email) VALUES
+  ('rahul dravid', 'bangalore', 'rahul.dravid@gmail.com'),
+  ('tom cuise', 'los angles', 'tom.cruise@gmail.com'),
+  ('kate', 'upton', 'kate.upton@gmail.com');
+  
